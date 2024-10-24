f7c657159f86fd41b540ce317d67a99ba2b675bb
Ryan Curry <32741719+rcurry16@users.noreply.github.com>
2024-10-23 21:07:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
